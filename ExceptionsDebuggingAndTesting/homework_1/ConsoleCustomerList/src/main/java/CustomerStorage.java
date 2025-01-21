2c78259419ba236b8826377fd52e05eb109b7bc3
FullFe <alexprokofiev03@gmail.com>
2025-01-21 21:27:20 +0300

    18.9 Практическая работа

--- ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -1,3 +1,6 @@
+import CustomExceptions.IncorrectInputDataException;
+import CustomExceptions.WrongNumOfParamException;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,14 +11,31 @@ public class CustomerStorage {
         storage = new HashMap<>();
     }
 
-    public void addCustomer(String data) {
+    public void addCustomer(String data) throws RuntimeException{
         final int INDEX_NAME = 0;
         final int INDEX_SURNAME = 1;
         final int INDEX_EMAIL = 2;
         final int INDEX_PHONE = 3;
 
+        String regex1 = "[а-яА-ЯёЁ]+";
+        String regex2 = "[a-zA-Z0-9.]+@[a-zA-Z]+\\.[a-zA-Z]+";
+        String regexNumber="\\+7[0-9]+";
+
         String[] components = data.split("\\s+");
+        if (!(components.length == 4)){
+            throw new WrongNumOfParamException("Wrong number of parameters in add method");
+        }
         String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
+
+        if(!(components[INDEX_NAME].matches(regex1) && components[INDEX_SURNAME].matches(regex1))){
+            throw new IncorrectInputDataException("Incorrect name/surname format");
+        }
+        if(!(components[INDEX_EMAIL].matches(regex2))){
+            throw new IncorrectInputDataException("Incorrect email format");
+        }
+        if(!(components[INDEX_PHONE].matches(regexNumber) && components[INDEX_PHONE].length()== 12)){
+            throw new IncorrectInputDataException("Incorrect phone format");
+        }
         storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
     }
 
--- ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/Main.java
+++ ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/Main.java
@@ -1,3 +1,10 @@
+import CustomExceptions.IncorrectInputDataException;
+import CustomExceptions.WrongNumOfParamException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.Scanner;
 
 public class Main {
@@ -9,26 +16,57 @@ public class Main {
             COMMAND_EXAMPLES;
     private static final String helpText = "Command examples:\n" + COMMAND_EXAMPLES;
 
+    private static final Logger querryLogger = LogManager.getLogger("QueryLogger");
+    private static final Logger errorLogger = LogManager.getLogger("ErrorLogger");
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         CustomerStorage executor = new CustomerStorage();
-
+        querryLogger.info("Starting main thread");
         while (true) {
             String command = scanner.nextLine();
             String[] tokens = command.split("\\s+", 2);
 
             if (tokens[0].equals("add")) {
-                executor.addCustomer(tokens[1]);
+                try{
+                    executor.addCustomer(tokens[1]);
+                    querryLogger.debug("Customer added: {}", tokens[1]);
+                }catch (WrongNumOfParamException | IncorrectInputDataException  | ArrayIndexOutOfBoundsException e){
+                    e.printStackTrace();
+                    errorLogger.error(e.getMessage());
+                    StringWriter sw = new StringWriter();
+                    e.printStackTrace(new PrintWriter(sw));
+                    String stackTraceString = sw.toString();
+                    errorLogger.error(stackTraceString);
+                }
             } else if (tokens[0].equals("list")) {
                 executor.listCustomers();
+                querryLogger.debug("List showed to user");
             } else if (tokens[0].equals("remove")) {
-                executor.removeCustomer(tokens[1]);
+                try{
+                    executor.removeCustomer(tokens[1]);
+                    querryLogger.debug("Customer deleted: {}", tokens[1]);
+                }catch (IncorrectInputDataException  | ArrayIndexOutOfBoundsException e){
+                    e.printStackTrace();
+                    errorLogger.error(e.getMessage());
+                    StringWriter sw = new StringWriter();
+                    e.printStackTrace(new PrintWriter(sw));
+                    String stackTraceString = sw.toString();
+                    errorLogger.error(stackTraceString);
+                }
+
             } else if (tokens[0].equals("count")) {
                 System.out.println("There are " + executor.getCount() + " customers");
+                querryLogger.debug("Amount of customers in the list: {}", executor.getCount());
             } else if (tokens[0].equals("help")) {
                 System.out.println(helpText);
-            } else {
+                querryLogger.info("Main thread exits");
+            } else if ("stop".equals(tokens[0])) {
+                querryLogger.info("Stopping main thread");
+                break;
+            }else {
                 System.out.println(COMMAND_ERROR);
+                querryLogger.warn("User look for help");
             }
         }
     }
