d0201d7bb614db71f9869fae98e2c58ce57a6557
FullFe <alexprokofiev03@gmail.com>
2025-02-11 00:16:07 +0300

    21.4 Практическая работа №1

--- Multithreading/ImageResizer/src/Main.java
+++ Multithreading/ImageResizer/src/Main.java
@@ -1,51 +1,54 @@
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.File;
 
 public class Main {
 
-    public static void main(String[] args) {
-        String srcFolder = "/users/sortedmap/Desktop/src";
-        String dstFolder = "/users/sortedmap/Desktop/dst";
+    private final static int CORE_AMOUNT = 6;
 
+    public static void main(String[] args) {
+        /*
+        Поменять эти строки на нужные
+         */
+//        String srcFolder = "/users/sortedmap/Desktop/src";
+//        String dstFolder = "/users/sortedmap/Desktop/dst";
+
+        String srcFolder = "C:/Users/alexp/OneDrive/Рабочий стол/PICS_N_VIDS/photo_from_army";
+        String dstFolder = "C:/Users/alexp/OneDrive/Рабочий стол/PICS_N_VIDS/newArmy";
         File srcDir = new File(srcFolder);
 
         long start = System.currentTimeMillis();
 
         File[] files = srcDir.listFiles();
 
-        try {
-            for (File file : files) {
-                BufferedImage image = ImageIO.read(file);
-                if (image == null) {
-                    continue;
-                }
+        if (files != null) {
+            File[][] chunks = splitArray(files);
 
-                int newWidth = 300;
-                int newHeight = (int) Math.round(
-                    image.getHeight() / (image.getWidth() / (double) newWidth)
-                );
-                BufferedImage newImage = new BufferedImage(
-                    newWidth, newHeight, BufferedImage.TYPE_INT_RGB
-                );
-
-                int widthStep = image.getWidth() / newWidth;
-                int heightStep = image.getHeight() / newHeight;
-
-                for (int x = 0; x < newWidth; x++) {
-                    for (int y = 0; y < newHeight; y++) {
-                        int rgb = image.getRGB(x * widthStep, y * heightStep);
-                        newImage.setRGB(x, y, rgb);
-                    }
-                }
-
-                File newFile = new File(dstFolder + "/" + file.getName());
-                ImageIO.write(newImage, "jpg", newFile);
+            for (File[] chunk : chunks) {
+                new ImageResizer(chunk,start,dstFolder).start();
             }
-        } catch (Exception ex) {
-            ex.printStackTrace();
         }
 
-        System.out.println("Duration: " + (System.currentTimeMillis() - start));
+    }
+
+    public static File[][] splitArray(File[] array) {
+
+        int chunkSize = array.length / CORE_AMOUNT;
+        int remainder = array.length % CORE_AMOUNT;
+        File[][] chunks = new File[CORE_AMOUNT][];
+        int count = 0;
+        for (int i = 0; i < CORE_AMOUNT; i++) {
+
+            int arrSize = chunkSize;
+            if(remainder > 0) {
+                arrSize++;
+                remainder--;
+            }
+            File[] chunk = new File[arrSize];
+            for (int j = 0; j < arrSize; j++) {
+                chunk[j] = array[count++];
+            }
+            chunks[i] = chunk;
+        }
+
+        return chunks;
     }
 }
